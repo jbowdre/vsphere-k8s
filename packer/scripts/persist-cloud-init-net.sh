87b627ebcb939c3ecc6def3f79b3688471428ec1
John Bowdre <john@bowdre.net>
2022-12-13 15:25:05 -0600

    script cleanup

--- packer/scripts/persist-cloud-init-net.sh
+++ packer/scripts/persist-cloud-init-net.sh
@@ -1,3 +1,7 @@
 #!/bin/sh -eu
 echo '>> Preserving network settings...'
-echo 'manual_cache_clean: True' | sudo tee -a /etc/cloud/cloud.cfg
+if grep -q 'manual_cache_clean' /etc/cloud/cloud.cfg; then
+  sudo sed -i 's/^manual_cache_clean.*$/manual_cache_clean: True/' /etc/cloud/cloud.cfg
+else
+  echo 'manual_cache_clean: True' | sudo tee -a /etc/cloud/cloud.cfg
+fi
